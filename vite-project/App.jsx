910f759b768fe7e608996af0918741cfff824257
panovaanastasiay <143810906+panovaanastasiay@users.noreply.github.com>
2024-04-04 19:42:31 -0700

    Add files via upload

---
+++ vite-project/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Item from './components/Item'
+import './App.css'
+
+function App() {
+  const item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+  }
+  return <Item info ={item}/>;
+}
+
+export default App
